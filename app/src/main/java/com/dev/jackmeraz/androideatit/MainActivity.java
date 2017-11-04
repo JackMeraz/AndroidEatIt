9599ffac8c99ea33f3895faa3356c8ef22f843b7
Jacobo Meraz <jacobo.meraz@utrodeo.edu.mx>
2017-11-04 13:14:46 -0600

    Initial commit

---
+++ app/src/main/java/com/dev/jackmeraz/androideatit/MainActivity.java
@@ -0,0 +1,42 @@
+package com.dev.jackmeraz.androideatit;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnRegistrar, btnLogin;
+    TextView txtSlogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
+        txtSlogan = (TextView) findViewById(R.id.txtslogan);
+
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/NABILA.TTF");
+        txtSlogan.setTypeface(face);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
